97689a2d8c9e08707e6ee2cf05c3ddebc5c61409
chaitanyarajugithub <chaitanyaraju2@gmail.com>
2019-02-28 13:43:34 +0530

    Added

--- src/main/java/PageClass_Admin/Users.java
+++ src/main/java/PageClass_Admin/Users.java
@@ -10,11 +10,33 @@ public class Users {
 	{
 	this.driver=driver;
 	}
-	@FindBy(xpath="//a[@href='/admin/users']")
-	WebElement Users;
+	@FindBy(xpath="//input[@name='admin_user[email]']")
+	WebElement EnterAdminEmail;
 	@FindBy(xpath="//input[@id='admin_user_password']")
 	WebElement EnterAdminPassword;
 	@FindBy(xpath="//input[@value='Login']")
 	WebElement AdminLogin;
+	@FindBy(xpath="//a[@href='/admin/users']")
+	WebElement Users;
+	@FindBy(xpath="//input[@id='q_email']")
+	WebElement Emailsearch;
+	@FindBy(xpath="//input[@id='q_personal_email']")
+	WebElement Personalemailsearch;
+	@FindBy(xpath="//input[@value='Filter']")
+	WebElement Filter;
+	@FindBy(xpath="//a[@class='clear_filters_btn']")
+	WebElement ClearFilter;
+	@FindBy(xpath="//span[@class='blank_slate']")
+	WebElement Nousersfound;
+	@FindBy(xpath="//div[@class='pagination_information']")
+	WebElement Displayusers;
+	@FindBy(xpath="//a[@title='Edit']")
+	WebElement Edit;
+	@FindBy(xpath="//input[@name='user[password]']")
+	WebElement password;
+	@FindBy(xpath="//input[@id='user_password_confirmation']")
+	WebElement confirmpassword;
+	@FindBy(xpath="//input[@value='Update User']")
+	WebElement Updateuser;
 
 }
