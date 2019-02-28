6e1f0d4dffaf8cc7caab2c532ade6095a6eb9169
chaitanyarajugithub <chaitanyaraju2@gmail.com>
2019-02-28 13:18:15 +0530

    Added

--- src/main/java/PageClass_Admin/Admin_Login.java
+++ src/main/java/PageClass_Admin/Admin_Login.java
@@ -1,5 +1,29 @@
 package PageClass_Admin;
 
-public class Admin_Login {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class Admin_Login {
+	WebDriver driver;
+	public Admin_Login(WebDriver driver)
+	{
+	this.driver=driver;
+	}
+	@FindBy(xpath="//input[@name='admin_user[email]']")
+	WebElement EnterAdminEmail;
+	@FindBy(xpath="//input[@id='admin_user_password']")
+	WebElement EnterAdminPassword;
+	@FindBy(xpath="//input[@value='Login']")
+	WebElement AdminLogin;
+	
+	public void Loginuser(String email,String password) {
+		
+		this.EnterAdminEmail.clear();
+		this.EnterAdminEmail.sendKeys(email);
+		this.EnterAdminPassword.clear();
+		this.EnterAdminPassword.sendKeys(password);
+		this.AdminLogin.click();		
+	
+	}
 }
